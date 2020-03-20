6a845a291490aa02b513815c4a1dfa961fd03394
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2020-03-20 14:46:24 +0000

    Added Scripts for create lect

--- public/js/app.js
+++ public/js/app.js
@@ -2566,7 +2566,56 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
-/* harmony default export */ __webpack_exports__["default"] = ({});
+/* harmony default export */ __webpack_exports__["default"] = ({
+  data: function data() {
+    return {
+      form: {
+        name: "",
+        address: "",
+        email: "",
+        phone: ""
+      },
+      loggedIn: false,
+      errors: []
+    };
+  },
+  computed: {
+    phoneValid: function phoneValid() {
+      return this.form.code.length <= 11 && this.form.code.length > 0;
+    }
+  },
+  created: function created() {
+    if (localStorage.getItem('token')) {
+      this.loggedIn = true;
+    } else {
+      this.loggedIn = false;
+      app.$router.push('/');
+    }
+  },
+  methods: {
+    onSubmit: function onSubmit(evt) {
+      evt.preventDefault();
+      var app = this;
+      var token = localStorage.getItem('token');
+      axios.post('/api/lecturers', {
+        name: app.form.name,
+        address: app.form.address,
+        email: app.form.email,
+        phone: app.form.phone
+      }, {
+        headers: {
+          Authorization: "Bearer ".concat(token)
+        }
+      }).then(function (response) {
+        console.log(response);
+        app.$router.push('/courses');
+      })["catch"](function (error) {
+        console.log(error.response.data);
+        app.errors = errors.response.data.errors;
+      });
+    }
+  }
+});
 
 /***/ }),
 
--- resources/js/views/lecturers/Create.vue
+++ resources/js/views/lecturers/Create.vue
@@ -4,6 +4,59 @@
 
 <script>
 export default {
-    
+    data() {
+        return {
+            form: {
+                name: "",
+                address: "",
+                email: "",
+                phone: "",
+            },
+            loggedIn: false,
+            errors: []
+        }
+    },
+
+    computed: {
+        phoneValid() {
+            return this.form.code.length <= 11 && this.form.code.length > 0
+        }
+    },
+
+    created() {
+        if(localStorage.getItem('token')) {
+            this.loggedIn = true;
+        } else {
+            this.loggedIn = false;
+            app.$router.push('/');
+        }
+    },
+
+    methods: {
+        onSubmit(evt) {
+            evt.preventDefault()
+
+            let app = this;
+            let token = localStorage.getItem('token');
+
+            axios.post('/api/lecturers', {
+                name: app.form.name,
+                address: app.form.address,
+                email: app.form.email,
+                phone: app.form.phone,
+            },
+            {
+                headers: { Authorization: `Bearer ${token}` }
+            })
+            .then(function(response) {
+                console.log(response);
+                app.$router.push('/courses');
+            })
+            .catch(function(error) {
+                console.log(error.response.data);
+                app.errors = errors.response.data.errors
+            })
+        }
+    }
 }
 </script>
