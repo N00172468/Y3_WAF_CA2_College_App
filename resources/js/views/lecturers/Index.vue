ebf58932f4bcfd1f1919d6d92bc2847b5970d68f
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2020-03-20 13:23:32 +0000

    Added Scripts for Lects API

--- resources/js/views/lecturers/Index.vue
+++ resources/js/views/lecturers/Index.vue
@@ -4,6 +4,27 @@
 
 <script>
 export default {
-    
+    data() {
+        return {
+            items: []
+        }
+    },
+
+    created() {
+        let app = this;
+        let token = localStorage.getItem('token');
+        axios.get('/api/lecturers', {
+            headers: {
+                Authorization: "Bearer " + token
+            }
+        })
+        .then(function (response) {
+            console.log(response.data);
+            app.items = response.data.data;
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+    }
 }
 </script>
