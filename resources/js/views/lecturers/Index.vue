d47fd6899bdaaf0ac8cc302814c797c25fa67ca2
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2020-03-20 13:42:57 +0000

    Created View for Lects Index

--- resources/js/views/lecturers/Index.vue
+++ resources/js/views/lecturers/Index.vue
@@ -1,5 +1,31 @@
 <template>
-    
+    <b-row align-h="center">
+        <b-col cols="12">
+            <!-- Table -->
+            <b-table-simple hover responsive>
+                <!-- Headers -->
+                <b-head>
+                    <b-tr>
+                        <b-th>Name</b-th>
+                        <b-th>Address</b-th>
+                        <b-th>Email</b-th>
+                        <b-th>Phone</b-th>
+                    </b-tr>
+                </b-head>
+
+                <!-- Body -->
+                <b-body>
+                    <b-tr v-for="item in items" :key="item.id">
+                        <b-td>{{ item.name }}</b-td>
+                        <b-td>{{ item.address }}</b-td>
+                        <b-td>{{ item.email }}</b-td>
+                        <b-td>{{ item.phone }}</b-td>
+                        <b-td><router-link :to="`/lecturers/edit/${item.id}`">Edit</router-link></b-td>
+                    </b-tr>
+                </b-body>
+            </b-table-simple>
+        </b-col>
+    </b-row>
 </template>
 
 <script>
