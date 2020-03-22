23957da7c50353d93a2f947fea0408884fa940c8
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2020-03-22 16:23:33 +0000

    SUCCESSFULLY Redesigned Edit Lects Form

--- resources/js/views/lecturers/Edit.vue
+++ resources/js/views/lecturers/Edit.vue
@@ -1,17 +1,131 @@
 <template>
-    <b-row align-h="center">
+<el-row style="margin:auto;">
+    <el-col>
+
+        <h3 v-if="!loggedIn">You are not logged in!!</h3>
+
+        <!-- Card -->
+        <el-card v-else class="box-card">
+        
+            <!-- Card Title -->
+            <div slot="header" class="clearfix">
+                <span>Edit Lecturer</span>
+            </div>
+
+            <!-- Form -->
+            <el-form 
+                @submit="onSubmit"
+                :model="form" 
+                :rules="rules"
+                ref="form" 
+                class="demo-ruleForm">
+            
+                <!-- Row 1 -->
+                <el-row>
+                    <!-- Name -->
+                    <el-col :span="24">
+                        <el-form-item 
+                            id="input-group-1"
+                            label-for="input-1" 
+                            prop="name" 
+                            label="Name">
+                            
+                            <el-input 
+                                id="input-1"
+                                type="text"
+                                placeholder="Name" 
+                                v-model="lecture.name">
+                            </el-input>
+                        </el-form-item>
+                    </el-col>
+                </el-row>
+
+                <!-- Row 2 -->
+                <el-row>
+                    <!-- Address -->
+                    <el-col :span="24">
+                        <el-form-item 
+                            id="input-group-2"
+                            label-for="input-2" 
+                            prop="address" 
+                            label="Address">
+                            
+                            <el-input 
+                                id="input-2"
+                                type="textarea"
+                                placeholder="Address" 
+                                v-model="lecture.address">
+                            </el-input>
+                        </el-form-item>
+                    </el-col>
+                </el-row>
+
+                <!-- Row 3 -->
+                <el-row :gutter="50">
+                    <!-- Email -->
+                    <el-col :span="12">
+                        <el-form-item 
+                            id="input-group-3"
+                            label-for="input-3" 
+                            prop="email" 
+                            label="Email">
+                            
+                            <el-input 
+                                style="width:350px;"
+                                id="input-3"
+                                type="email"
+                                placeholder="Email" 
+                                v-model="lecture.email">
+                            </el-input>
+                        </el-form-item>
+                    </el-col>
+
+                    <!-- Phone -->
+                    <el-col :span="12">
+                        <el-form-item 
+                            id="input-group-4"
+                            label-for="input-4"
+                            prop="phone" 
+                            label="Phone Number">
+                            
+                            <el-input 
+                                style="width:350px;"
+                                id="input-4"
+                                type="text"
+                                placeholder="Phone Number" 
+                                v-model="lecture.phone">
+                            </el-input>
+                        </el-form-item>
+                    </el-col>
+                </el-row>
+
+                <!-- Row 4 -->
+                <el-row>
+                    <el-col :span="24">
+                        <el-form-item>
+                            <el-button @click="onSubmit" type="primary">Save</el-button>
+                        </el-form-item>
+                    </el-col>
+                </el-row>
+            </el-form>
+
+        </el-card>
+    
+    </el-col>
+</el-row>
+    <!-- <b-row align-h="center">
         <b-col cols="8">
             <h3 v-if="!loggedIn">You are not logged in!</h3>
 
             <b-card
                 v-else
                 title="Edit Lecture"
-                tag="article">
+                tag="article"> -->
 
                 <!-- Form -->
-                <b-form @submit="onSubmit">
+                <!-- <b-form @submit="onSubmit"> -->
                     <!-- Name -->
-                    <b-form-group
+                    <!-- <b-form-group
                         id="input-group-1"
                         label="Name"
                         label-for="input-1">
@@ -23,10 +137,10 @@
                             placeholder="Name"
                             required>
                         </b-form-input>
-                    </b-form-group>
+                    </b-form-group> -->
 
                     <!-- Address -->
-                    <b-form-group
+                    <!-- <b-form-group
                         id="input-group-2"
                         label="Address"
                         label-for="input-2">
@@ -38,10 +152,10 @@
                             placeholder="Address"
                             required>
                         </b-form-input>
-                    </b-form-group>
+                    </b-form-group> -->
 
                     <!-- Email -->
-                    <b-form-group
+                    <!-- <b-form-group
                         id="input-group-3"
                         label="Email"
                         label-for="input-3">
@@ -53,10 +167,10 @@
                             placeholder="Email"
                             required>
                         </b-form-input>
-                    </b-form-group>
+                    </b-form-group> -->
 
                     <!-- Phone -->
-                    <b-form-group
+                    <!-- <b-form-group
                         id="input-group-4"
                         label="Phone Number"
                         label-for="input-4">
@@ -68,10 +182,10 @@
                             placeholder="Phone Number"
                             required>
                         </b-form-input>
-                    </b-form-group>
+                    </b-form-group> -->
 
                     <!-- Submit Button -->
-                    <b-button 
+                    <!-- <b-button 
                         type="submit"
                         variant="primary"
                         >
@@ -80,7 +194,7 @@
                 </b-form>
             </b-card>
         </b-col>
-    </b-row>
+    </b-row> -->
 </template>
 
 <script>
