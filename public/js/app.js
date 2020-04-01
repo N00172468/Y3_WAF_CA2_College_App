fceb2d2ce914039d3140f1ad6886a14540f48b1d
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2020-04-01 12:24:11 +0100

    Clean up

--- public/js/app.js
+++ public/js/app.js
@@ -4722,33 +4722,6 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
 
 /* harmony default export */ __webpack_exports__["default"] = ({
   data: function data() {
--- resources/js/views/courses/Index.vue
+++ resources/js/views/courses/Index.vue
@@ -101,33 +101,6 @@
 
   </el-col>
 </el-row>
-  <!-- <b-row align-h="center">
-    <b-col cols="12">
-
-      <b-table-simple hover responsive>
-        <b-head>
-          <b-tr>
-            <b-th>Title</b-th>
-            <b-th>Code</b-th>
-            <b-th>Description</b-th>
-            <b-th>Points</b-th>
-            <b-th>Level</b-th>
-            <b-th>Actions</b-th>
-          </b-tr>
-        </b-head>
-        <b-body>
-          <b-tr v-for="item in items" :key="item.id">
-            <b-td>{{ item.title }}</b-td>
-            <b-td>{{ item.code }}</b-td>
-            <b-td>{{ item.description }}</b-td>
-            <b-td>{{ item.level }}</b-td>
-            <b-td>{{ item.points }}</b-td>
-            <b-td><router-link :to="`/courses/edit/${item.id}`">Edit</router-link></b-td>
-          </b-tr>
-        </b-body>
-      </b-table-simple>
-    </b-col>
-  </b-row> -->
 </template>
 
 <script>
