7117f3d51a4de8171f0ed6eaf24c17cbe9ec82c1
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2020-04-01 12:27:41 +0100

    Clean up

--- public/js/app.js
+++ public/js/app.js
@@ -5503,33 +5503,6 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
 
 /* harmony default export */ __webpack_exports__["default"] = ({
   data: function data() {
