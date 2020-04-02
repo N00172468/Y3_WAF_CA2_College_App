a04235cfe51da4391253656d86b77975c3d95fcc
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2020-04-02 18:13:48 +0100

    Imported Element UI custom dark theme

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,12 @@
 require('./bootstrap');
 import Vue from 'vue'
 import App from './App.vue'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import locale from 'element-ui/lib/locale/lang/en'
-// import BootstrapVue from 'bootstrap-vue'
-// import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'element-theme-dark'
 import router from './router'
 
-// Vue.use(BootstrapVue)
 Vue.use(ElementUI, { locale })
 
 new Vue({
