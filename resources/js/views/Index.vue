f96225037f8df7f024c65aa594fd231dc4378417
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2020-04-03 17:48:52 +0100

    Replaced hard-coded Timestamp with its component.

--- resources/js/views/Index.vue
+++ resources/js/views/Index.vue
@@ -25,43 +25,7 @@
     
       <!-- Static Timestamp (For Aesthetics Only (Filler)) -->
       <el-aside width="50%">
-        <div class="block">
-          <el-timeline>
-
-            <!-- Timestamp 1 -->
-            <el-timeline-item type="success" timestamp="1/1/2020" placement="top">
-              <el-card>
-                <h4>Test 1</h4>
-                <p>Ipsy Lipsy Shimmy Shimmy Yaaa</p>
-              </el-card>
-            </el-timeline-item>
-
-            <!-- Timestamp 2 -->
-            <el-timeline-item type="success" timestamp="15/2/2020" placement="top">
-              <el-card>
-                <h4>Test 2</h4>
-                <p>Ipsy Lipsy Shimmy Shimmy Yaaa</p>
-              </el-card>
-            </el-timeline-item>
-
-            <!-- Timestamp 3 -->
-            <el-timeline-item type="warning" timestamp="21/3/2020" placement="top">
-              <el-card>
-                <h4>Test 3</h4>
-                <p>Ipsy Lipsy Shimmy Shimmy Yaaa</p>
-              </el-card>
-            </el-timeline-item>
-
-            <!-- Timestamp 4 -->
-            <el-timeline-item type="danger" timestamp="1/4/2020" placement="top">
-              <el-card>
-                <h4>Test 4</h4>
-                <p>Ipsy Lipsy Shimmy Shimmy Yaaa</p>
-              </el-card>
-            </el-timeline-item>
-
-          </el-timeline>
-        </div>
+        <Timestamp />
       </el-aside>
 
       <!-- Calendar -->
@@ -148,6 +112,7 @@
 <script>
 import Accordion from '../components/Accordion'
 import Carousel from '../components/Carousel'
+import Timestamp from '../components/Timestamp'
 
 export default {
   name: 'index',
@@ -214,7 +179,8 @@ export default {
 
   components: {
     Accordion,
-    Carousel
+    Carousel,
+    Timestamp
   }
 }
 </script>
