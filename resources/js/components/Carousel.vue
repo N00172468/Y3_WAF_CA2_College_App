1e21054d032eaf01c4e6282d934fa0c9632d63a4
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2020-04-03 17:19:48 +0100

    Designed Carousel component

--- resources/js/components/Carousel.vue
+++ resources/js/components/Carousel.vue
@@ -1,8 +1,27 @@
 <template>
-    
+    <el-carousel :interval="3000" type="card" height="600px">
+        <!-- Course Table -->
+        <el-carousel-item>
+            <IndexCourse/>
+        </el-carousel-item>
+
+        <!-- Lects Table -->
+        <el-carousel-item>
+            <IndexLects/>
+        </el-carousel-item>
+
+        <!-- Enrol Table -->
+        <el-carousel-item>
+            <IndexEnrol/>
+        </el-carousel-item>
+    </el-carousel>
 </template>
 
 <script>
+import IndexCourse from './views/courses/Index'
+import IndexLects from './views/lecturers/Index'
+import IndexEnrol from './views/enrolments/Index'
+
 export default {
     
 }
