7c93a9276d3fabec18f4b6312e2cae6ad983d689
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2020-04-03 17:21:44 +0100

    Designed Timestamp component

--- resources/js/components/Timestamp.vue
+++ resources/js/components/Timestamp.vue
@@ -1,5 +1,41 @@
 <template>
-    
+    <div class="block">
+            <el-timeline>
+
+                <!-- Timestamp 1 -->
+                <el-timeline-item type="success" timestamp="1/1/2020" placement="top">
+                    <el-card>
+                        <h4>Test 1</h4>
+                        <p>Ipsy Lipsy Shimmy Shimmy Yaaa</p>
+                    </el-card>
+                </el-timeline-item>
+
+                <!-- Timestamp 2 -->
+                <el-timeline-item type="success" timestamp="15/2/2020" placement="top">
+                    <el-card>
+                        <h4>Test 2</h4>
+                        <p>Ipsy Lipsy Shimmy Shimmy Yaaa</p>
+                    </el-card>
+                </el-timeline-item>
+
+                <!-- Timestamp 3 -->
+                <el-timeline-item type="warning" timestamp="21/3/2020" placement="top">
+                    <el-card>
+                        <h4>Test 3</h4>
+                        <p>Ipsy Lipsy Shimmy Shimmy Yaaa</p>
+                    </el-card>
+                </el-timeline-item>
+
+                <!-- Timestamp 4 -->
+                <el-timeline-item type="danger" timestamp="1/4/2020" placement="top">
+                    <el-card>
+                        <h4>Test 4</h4>
+                        <p>Ipsy Lipsy Shimmy Shimmy Yaaa</p>
+                    </el-card>
+                </el-timeline-item>
+
+            </el-timeline>
+        </div>
 </template>
 
 <script>
