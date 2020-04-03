ebc00d86151da38d89037dd30f75012975c78bd2
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2020-04-03 17:04:15 +0100

    Designed Accordion component

--- resources/js/components/Accordion.vue
+++ resources/js/components/Accordion.vue
@@ -1,5 +1,42 @@
 <template>
-    
+    <el-collapse accordion>
+            
+            <!-- About the Project -->
+            <el-collapse-item title="About the Project" name="1">
+                <div>
+                    Ipsum Lipsum Lipsy Ipsy
+                </div>
+            </el-collapse-item>
+
+            <!-- Dashboard -->
+            <el-collapse-item title="Dashboard" name="2">
+                <div>
+                    Ipsum Lipsum Lipsy Ipsy
+                </div>
+            </el-collapse-item>
+
+            <!-- CRUD -->
+            <el-collapse-item title="C.R.U.D Functionalities" name="3">
+                <div>
+                    Ipsum Lipsum Lipsy Ipsy
+                </div>
+            </el-collapse-item>
+
+            <!-- Vue.js -->
+            <el-collapse-item title="Vue.js" name="4">
+                <div>
+                    Ipsum Lipsum Lipsy Ipsy
+                </div>
+            </el-collapse-item>
+
+            <!-- Element UI -->
+            <el-collapse-item title="Element U.I" name="5">
+                <div>
+                    Ipsum Lipsum Lipsy Ipsy
+                </div>
+            </el-collapse-item>
+        
+        </el-collapse>
 </template>
 
 <script>
