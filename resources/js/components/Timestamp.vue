4edaa218f9a8f9042e648e6233d15f6474a9ca6f
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2020-04-03 19:25:56 +0100

    Added realistic dummy text for Timestamp

--- resources/js/components/Timestamp.vue
+++ resources/js/components/Timestamp.vue
@@ -3,34 +3,34 @@
             <el-timeline>
 
                 <!-- Timestamp 1 -->
-                <el-timeline-item type="success" timestamp="1/1/2020" placement="top">
+                <el-timeline-item type="success" timestamp="6/12/2019" placement="top">
                     <el-card>
-                        <h4>Test 1</h4>
-                        <p>Ipsy Lipsy Shimmy Shimmy Yaaa</p>
+                        <h4>Courses</h4>
+                        <p>Successfully added a new course, edited an existing course and deleted an outdated course. New Course: UI/UX Design. Edited Course: Creative Computing. Deleted Course: Product (Industrial) Design.</p>
                     </el-card>
                 </el-timeline-item>
 
                 <!-- Timestamp 2 -->
-                <el-timeline-item type="success" timestamp="15/2/2020" placement="top">
+                <el-timeline-item type="success" timestamp="15/1/2020" placement="top">
                     <el-card>
-                        <h4>Test 2</h4>
-                        <p>Ipsy Lipsy Shimmy Shimmy Yaaa</p>
+                        <h4>Lecturers</h4>
+                        <p>Successfully recruited an additional lecturer, edited lecturer details and deleted information of a retired lecturer. New Lecturer: John Doe. Edited Lecturer: Joe Bloggs. Deleted Lecturer: John Joe.</p>
                     </el-card>
                 </el-timeline-item>
 
                 <!-- Timestamp 3 -->
                 <el-timeline-item type="warning" timestamp="21/3/2020" placement="top">
                     <el-card>
-                        <h4>Test 3</h4>
-                        <p>Ipsy Lipsy Shimmy Shimmy Yaaa</p>
+                        <h4>Enrolments</h4>
+                        <p>Must create new enrolment for John Doe in UI/UX Design. Due on 1/4/2020!</p>
                     </el-card>
                 </el-timeline-item>
 
                 <!-- Timestamp 4 -->
-                <el-timeline-item type="danger" timestamp="1/4/2020" placement="top">
+                <el-timeline-item type="danger" timestamp="22/3/2020" placement="top">
                     <el-card>
-                        <h4>Test 4</h4>
-                        <p>Ipsy Lipsy Shimmy Shimmy Yaaa</p>
+                        <h4>ALERT!</h4>
+                        <p>A lecturer has retired. Please remove information of lecturer.</p>
                     </el-card>
                 </el-timeline-item>
 
