eb5fc37348c2a7cc624d5d50aacc84ce8206f348
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2020-04-04 13:01:03 +0100

    Added comments for clarification

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,15 +1,15 @@
-require('./bootstrap');
-import Vue from 'vue'
-import App from './App.vue'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-import locale from 'element-ui/lib/locale/lang/en'
-import 'element-theme-dark'
-import router from './router'
+require('./bootstrap'); // Although Bootstrap UI has been replaced with Element UI), this import is still needed due to jQuery and Axios.  
+import Vue from 'vue' // Globalise Vue.
+import App from './App.vue' // Globalise App.vue (main page).
+import ElementUI from 'element-ui' // Globalise Element UI.
+import 'element-ui/lib/theme-chalk/index.css' // Globalise Element UI components.
+import locale from 'element-ui/lib/locale/lang/en' // Translate default language (Mandarin) to English.
+import 'element-theme-dark' // Globalise bespoke Dark Theme.
+import router from './router' // Globalise Router.
 
-Vue.use(ElementUI, { locale })
+Vue.use(ElementUI, { locale }) // Allow Vue to use Element UI and its translated form.
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: h => h(App) 
+}).$mount('#app') // Render pages and their contents within App.vue 
