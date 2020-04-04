562a69ad7eccdefc5d7fbdd282004708f306b52a
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2020-04-04 16:33:34 +0100

    Added comments for clarification

--- resources/js/views/courses/Delete.vue
+++ resources/js/views/courses/Delete.vue
@@ -2,7 +2,7 @@
     <el-button
         @click="open"
         type="danger"
-        circle>
+        circle> <!-- "circle" is an Element UI attribute that transforms the button into a circular object -->
 
         <i class="el-icon-delete"></i>
     </el-button>
@@ -10,18 +10,11 @@
 
 <script>
 export default {
-    props: ['id'],
-
-    data() {
-        return {
-            course: [],
-            show: true,
-            loggedIn: false
-        }
-    },
+    props: ['id'], // Contain the array of ID as a prop
 
     methods: {
-        open() {
+        // Methods once Delete Modal has been opened.
+        open() { // Open Modal based on Element UI docs.
             this.$confirm('This will permanently delete the Course. Continue?', 'Warning', {
                 confrimButtonText: 'OK',
                 cancelButtonText: 'Cancel',
@@ -30,23 +23,22 @@ export default {
             .then(() => {
                 let app = this;
                 let token = localStorage.getItem('token');
-                let id = (this.$route.params.id) ? this.$route.params.id : this.id;
-                axios.delete(`/api/courses/${id}`, {
+                let id = (this.$route.params.id) ? this.$route.params.id : this.id; // This determines whether the user is deleting contents within the Index or Show page.
+
+                axios.delete(`/api/courses/${id}`, { // DELETE request within Axios. Used to allow the user to delete data from application and database.
                     headers: { Authorization: "Bearer " + token }
                 }).then(function(){
-                    app.$emit('row-deleted');
+                    app.$emit('row-deleted'); // This event allows to emit the method throughout other files (See Index and Show files).
                 })
                 
             })
-            .then(() => {
-                // app.$router.push('/courses');
-
+            .then(() => { // Display message once delete is successful.
                 this.$message({
                     type: 'success',
                     message: 'Delete Completed'
                 });
             })
-            .catch(() => {
+            .catch(() => { // Display message once delete has been cancelled.
                 this.$message({
                     type: 'info',
                     message: 'Delete cancelled'
