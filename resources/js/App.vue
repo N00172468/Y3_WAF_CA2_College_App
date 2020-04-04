2dfc356adb2dfd6320be7e8f9353d62fb20edda2
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2020-04-04 13:05:36 +0100

    Added comments for clarification

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,25 +1,21 @@
 <template>
   <div>
-    <MyNavbar />
+    <MyNavbar /> <!-- Render Navbar first per page (unless user is not logged in) -->
+
     <el-container>
-      <router-view />
+      <router-view /> <!-- Render selected pages mentioned in router file -->
     </el-container>
   </div>
 </template>
+
 <script>
-import MyNavbar from './components/MyNavbar'
+import MyNavbar from './components/MyNavbar' // Needed to use component
 
 export default {
   name: 'app',
-  components: {
-    MyNavbar
-  },
-  data() {
-    return {
 
-    }
+  components: { // Needed to display components
+    MyNavbar
   }
 }
 </script>
-<style>
-</style>
