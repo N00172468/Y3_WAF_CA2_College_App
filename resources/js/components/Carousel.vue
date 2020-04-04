e0bab14672d6aa564f65826c5873cd5c34137197
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2020-04-04 17:54:40 +0100

    Added comments for clarification

--- resources/js/components/Carousel.vue
+++ resources/js/components/Carousel.vue
@@ -1,18 +1,18 @@
 <template>
-    <el-carousel :interval="3000" type="card" height="600px">
+    <el-carousel :interval="3000" type="card" height="600px"> <!-- ":intervals" is the amount of miliseconds will pass until the next card rotates -->
         <!-- Course Table -->
         <el-carousel-item>
-            <IndexCourse />
+            <IndexCourse /> <!-- Displaying Courses Table -->
         </el-carousel-item>
 
         <!-- Lects Table -->
         <el-carousel-item>
-            <IndexLects />
+            <IndexLects /> <!-- Displaying Lecturers Table -->
         </el-carousel-item>
 
         <!-- Enrol Table -->
         <el-carousel-item>
-            <IndexEnrol />
+            <IndexEnrol /> <!-- Displaying Enrolments Table -->
         </el-carousel-item>
     </el-carousel>
 </template>
