b9cd80579e45e33586ccb3d809fb30fdaa502ad4
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2020-04-05 12:02:20 +0100

    Bug fixing secure https issue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
 
-        if(config('app.env') === 'production') {
+        if(config('APP_ENV') === 'production') {
             URL::forceScheme('https');
         }
     }
